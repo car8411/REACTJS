e40dc42c5cf890f904fbfb6b43ff772aa1806ef6
car8411 <car8411@naver.com>
2024-09-07 02:46:55 +0900

    git push

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import NavBar from './components/NavBar';
+
 import Home from './components/Home';
 import About from './components/About';
 import './styles/App.css';
