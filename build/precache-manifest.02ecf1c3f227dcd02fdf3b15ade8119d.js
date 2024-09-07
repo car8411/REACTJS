9bc72c61b3563bc9c8256d41eeba63fcde68afff
car8411 <car8411@naver.com>
2024-09-07 21:55:21 +0900

    asdasd

---
+++ build/precache-manifest.02ecf1c3f227dcd02fdf3b15ade8119d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "845b614818ea984c0e89fc28ace4e21f",
+    "url": "/REACTJS/index.html"
+  },
+  {
+    "revision": "4114278256bbc19bdb4d",
+    "url": "/REACTJS/static/css/main.986379e3.chunk.css"
+  },
+  {
+    "revision": "1ca3360b8d6f254a0c52",
+    "url": "/REACTJS/static/js/2.4e72292d.chunk.js"
+  },
+  {
+    "revision": "cbf92ed71fb4f2e94692ba67215d89b7",
+    "url": "/REACTJS/static/js/2.4e72292d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4114278256bbc19bdb4d",
+    "url": "/REACTJS/static/js/main.ac9d7a8b.chunk.js"
+  },
+  {
+    "revision": "310d8cdb0694fea44574",
+    "url": "/REACTJS/static/js/runtime-main.1397505d.js"
+  }
+]);
--- src/components/Home.js
+++ src/components/Home.js
@@ -5,7 +5,20 @@ function Home() {
   return (
     <div>
       <h1>Home Page</h1>
-      <p>Welcome to the Home dddddddddddddddddddPage ddddde dddddddddddddddddddPage ddddde dddddddddddddddddddPage ddddde dddddddddddddddddddPage ddddde dddddddddddddddddddPage ddddde dddddddddddddddddddPage ddddde dddddddddddddddddddPage ddddde dddddddddddddddddddPage ddddde dddddddddddddddddddPage ddddde dddddddddddddddddddPage ddddde dddddddddddddddddddPage ddddde dddddddddddddddddddPage ddddde dddddddddddddddddddPage ddddde dddddddddddddddddddPage ddddde dddddddddddddddddddPage ddddde dddddddddddddddddddPage ddddde dddddddddddddddddddPage ddddde dddddddddddddddddddPage dddddddddddddddddddPage dddddddddddddddddddPage dddddddddddddddddddPage dddddddddddddddddddPage dddddddddddddddddddPage dddddddddddddddddddPage dddddddddddddddddddPage dddddddddddddddddddPage dddddddddddddddddddPage dddddddddddddddddddPage dddddddddddddddddddPage dddddddddddddddddddPage dddddddddddddddddddPage dddddddddddddddddddPage dddddddddddddddddddPage dddddddddddddddddddPage dddddddddddddddddddPage dddddddddddddddddddPage dddddddddddddddddddPage dddddddddddddddddddPage dddddddddddddddddddPage dddddddddddddddddddPage dddddddddddddddddddPage dddddddddddddddddddPage dddddddddddddddddddPage dddddddddddddddddddPage dddddddddddddddddddPage dddddddddddddddddddPage dddddddddddddddddddPage dddddddddddddddddddPage dddddddddddddddddddPage dddddddddddddddddddPage dddddddddddddddddddPage dddddddddddddddddddPage dddddddddddddddddddPage dddddddddddddddddddPage dddddddddddddddddddPage dddddddddddddddddddPage dddddddddddddddddddPage dddddddddddddddddddPage dddddddddddddddddddPage dddddddddddddddddddPage dddddddddddddddddddPage dddddddddddddddddddPage dddddddddddddddddddPage dddddddddddddddddddPage dddddddddddddddddddPage dddddddddddddddddddPage dddddddddddddddddddPage dddddddddddddddddddPage dddddddddddddddddddPage dddddddddddddddddddPage dddddddddddddddddddPage dddddddddddddddddddPage dddddddddddddddddddPage dddddddddddddddddddPage dddddddddddddddddddPage dddddddddddddddddddPage dddddddddddddddddddPage dddddddddddddddddddPage dddddddddddddddddddPage dddddddddddddddddddPage dddddddddddddddddddPage dddddddddddddddddddPage dddddddddddddddddddPage dddddddddddddddddddPage dddddddddddddddddddPage dddddddddddddddddddPage dddddddddddddddddddPage!</p>
+      <p>my-react-app/</p>
+      <p>├── public/</p>
+│         <p>├── index.html</p>
+│         <p>└── ...</p>
+<p>├── src/</p>
+<p>│   ├── App.js</p>
+<p>│   ├── index.js</p>
+<p>│   ├── components/</p>
+<p>│   │   ├── Header.js</p>
+<p>│   │   └── Footer.js</p>
+<p>│   └── styles/</p>
+<p>│       └── App.css</p>
+<p>├── package.json</p>
+<button>└── ...</button>
     </div>
   );
 }
