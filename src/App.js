6651cf45539f69877f5ded34bee0bc3d3ffc4f48
car8411 <car8411@naver.com>
2024-09-07 13:09:39 +0900

    asd

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { HashRouter as Router, Route, Routes } from 'react-router-dom';
-
 import Home from './components/Home';
 import About from './components/About';
 import './styles/App.css';
@@ -9,7 +8,6 @@ function App() {
   return (
     <Router>
       <div className="App">
-        <NavBar />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
