ebbe91fb3fff6644c9c4492c5f9c70c492ab95c8
car8411 <car8411@naver.com>
2024-09-07 22:12:47 +0900

    asddddddddddddddddddddddddddddddddddd

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/REACTJS/precache-manifest.02ecf1c3f227dcd02fdf3b15ade8119d.js"
+  "/precache-manifest.f198208f56fc2f2fe3cac8333c8f26c6.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/REACTJS/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
